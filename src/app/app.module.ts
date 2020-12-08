089ebc26ad5bacc02bdae611fac3eb18af185855
Jason <e0177109@comp.nus.edu.sg>
2020-12-08 13:59:51 +0800

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,36 @@ import { FormsModule }    from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
-import jwtDecode, { JwtPayload } from "jwt-decode";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CusFormComponent } from './components/cus-form/cus-form.component';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBwXL8N5IXD0FBBKu1sJHtTzEvPzUO4xgc",
+  authDomain: "react-template-521ee.firebaseapp.com",
+  databaseURL: "https://react-template-521ee.firebaseio.com",
+  projectId: "react-template-521ee",
+  storageBucket: "react-template-521ee.appspot.com",
+  messagingSenderId: "644150921085",
+  appId: "1:644150921085:web:db19fbd0e2f560e5e3ae14",
+  measurementId: "G-Y88XXJFD9Q"
+};
+
 
 @NgModule({
   declarations: [
@@ -28,6 +49,10 @@ import { CusFormComponent } from './components/cus-form/cus-form.component';
     NgbModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
     HttpClientModule,
     JwtModule.forRoot({
       config: {
